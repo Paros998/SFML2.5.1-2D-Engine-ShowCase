8dc793fad8133b52011c191847524fc0451fe268
Dominik Grudzień <62188218+shanodis@users.noreply.github.com>
2021-10-12 22:17:15 +0200

    Engine as singleton class

--- SnakeSFML/main.cpp
+++ SnakeSFML/main.cpp
@@ -1,8 +1,20 @@
 import general;
+
 using namespace consts;
 using namespace globalVars;
 
+import <iostream>;
+using namespace std;
+
+import SnakeEngine;
+using namespace engine;
+
 int main(void) {
+	Engine* en1 = Engine::getInstance();
+	Engine* en2 = Engine::getInstance();
+
+	cout << en1 << endl;
+	cout << en2 << endl;
 
 	return 0;
 }
