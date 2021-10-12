a58f8b61a219ffb6df7546b4c384d2cb21a29efd
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-10-12 22:33:24 +0200

    bugfix

--- SnakeSFML/main.cpp
+++ SnakeSFML/main.cpp
@@ -13,12 +13,6 @@ int main(void) {
 	Engine* en1 = Engine::getInstance();
 	Engine* en2 = Engine::getInstance();
 
-	cout << en1 << endl;
-	cout << en2 << endl;
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    
 	return 0;
 }
