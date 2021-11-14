fa4553ce04f0c53535d94ecb5ca1fd59238124b1
Dominik Grudzień <62188218+shanodis@users.noreply.github.com>
2021-11-15 00:19:51 +0100

    lab 1 (#12)
    * megre
    
    * sss
    
    * lab 1

--- SnakeSFML/main.cpp
+++ SnakeSFML/main.cpp
@@ -1,13 +1,28 @@
-
+#include <SFML/Graphics.hpp>
 import <iostream>;
 using namespace std;
 
 import SnakeEngine;
 using namespace engine;
 
+import Engine;
+using namespace graphicsEngine;
+
 int main(void) {
-	if (Engine::main() == 0)
-		Engine::release;
+	//if (Engine::main() == 0)
+		//Engine::release;
+
+	GraphicsEngine* engine = GraphicsEngine::getInstance();
+	engine->main(
+		400,
+		200,
+		sf::Color::Black,
+		true,
+		true,
+		true,
+		true,
+		true
+	);
 	
 	return 0;
 }
